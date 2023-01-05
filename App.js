a60cbfd86d288c45f9b109e790b75a5f3228777f
DAZDAEJG <92121569+DAZDAEJG@users.noreply.github.com>
2023-01-04 18:48:11 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
+
+import LoginScreen from "./screens/LoginScreen";
+import LoadingScreen from "./screens/LoadingScreen";
+import DashboardScreen from "./screens/DashboardScreen";
+
+import * as firebase from "firebase";
+import { firebaseConfig } from "./config";
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+} else {
+    firebase.app();
+}
+
+const AppSwitchNavigator = createSwitchNavigator({
+    LoadingScreen: LoadingScreen,
+    LoginScreen: LoginScreen,
+    DashboardScreen: DashboardScreen
+})
+
+const AppNavigator = createAppContainer(AppSwitchNavigator)
+
+export default function App() {
+    return (
+        <AppNavigator />
+    )
+}
+
+//npm install @react-navigation/drawer
+//yarn add @react-navigation/drawer
+//yarn add @react-navigation/material-bottom-tabs react-native-paper
+//yarn add react-native-responsive-fontsize
---
+++ config.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyACRfKvbrPda_WLn-kO7GiHozm4urWuSoY",
+    authDomain: "cfdyert-eb429.firebaseapp.com",
+    projectId: "cfdyert-eb429",
+    storageBucket: "cfdyert-eb429.appspot.com",
+    messagingSenderId: "108305031271",
+    appId: "1:108305031271:web:69a3c6bc2418cc435474c2"
+  };
+
+
+  //email: hola@gmail.com
+  //password: 1234
+  //fontsLoaded: false
+  //userSignedIn:false
+  //Crear Logout.js en nuestra carpeta de pantallas
